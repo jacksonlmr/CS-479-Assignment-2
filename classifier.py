1c0932904f93fe1e45a65552d838602e49f570c5
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2026-04-05 13:22:40 -0700

    Fixed Bayesian Classifier for case 3

--- classifier.py
+++ classifier.py
@@ -6,35 +6,40 @@ def bayesian_case_3(mu1, mu2, sigma1, sigma2, w1_data: np.array, w2_data: np.arr
     Returns 1 if instance is likely to belong to class 1, 2 if instance is likely to belong to class 2
     """
     # calculate x^t * W_i * x
-    x_Wi_x_1 = np.matrix.transpose(instance) @ (-0.5 * np.linalg.inv(sigma1)) @ instance
-    x_Wi_x_2 = np.matrix.transpose(instance) @ (-0.5 * np.linalg.inv(sigma2)) @ instance
+    x_Wi_x_1 = instance.T @ (-0.5 * np.linalg.inv(sigma1)) @ instance
+    x_Wi_x_2 = instance.T @ (-0.5 * np.linalg.inv(sigma2)) @ instance
 
     # calculate (w_i)^t 
     w1 = np.linalg.inv(sigma1) @ mu1
-    w1_t = np.matrix.transpose(w1)
+    w1_t = w1.T
 
     w2 = np.linalg.inv(sigma2) @ mu2
-    w2_t = np.matrix.transpose(w2)
-    # print(f"w1: {w1}\n")
-    # print(f"w1_t: {w1_t}\n")
-    # multiply (w_i)^t * x
-    w1_t_x = w1_t @ np.matrix.transpose(instance)
-    w2_t_x = w2_t @ np.matrix.transpose(instance)
-    # print(f"w1_t_x: {w1_t_x}\n")
-    # calculate ln(P(w_i))
-    total_data_size = w1_data.size + w2_data.size
-    p_w1 = w1_data.size / total_data_size
-    p_w2 = w2_data.size / total_data_size
+    w2_t = w2.T
 
+    # multiply (w_i)^t * x
+    w1_t_x = w1_t @ instance
+    w2_t_x = w2_t @ instance
+
+    # calculate P(w_i)
+    total_data_size = w1_data.shape[0] + w2_data.shape[0]
+    p_w1 = w1_data.shape[0] / total_data_size
+    p_w2 = w2_data.shape[0] / total_data_size
+
+    # calculate ln(P(w_i))
     ln_p_w1 = math.log(p_w1)
     ln_p_w2 = math.log(p_w2)
-    # print(f"ln_p_w1: {ln_p_w1}\n")
+
+    # calculate ln|sigma_i|
+    sign1, ln_sigma_1 = np.linalg.slogdet(sigma1)
+    ln_sigma_1 = sign1*ln_sigma_1
+    sign2, ln_sigma_2 = np.linalg.slogdet(sigma2)
+    ln_sigma_2 = sign2*ln_sigma_2
+
     # calculate w_i0
-    
-    # print(f"sigma1: {sigma1}\n")
-    w_10 = (-(0.5 * (np.matrix.transpose(mu1) @ np.linalg.inv(sigma1))) @ mu1) + ln_p_w1
-    w_20 = (-(0.5 * (np.matrix.transpose(mu2) @ np.linalg.inv(sigma2))) @ mu2) + ln_p_w2
-    # print(f"w_10: {w_10}\n")
+    w_10 = (-(0.5 * (mu1.T @ np.linalg.inv(sigma1))) @ mu1) - 0.5 * ln_sigma_1 + ln_p_w1
+    w_20 = (-(0.5 * (mu2.T @ np.linalg.inv(sigma2))) @ mu2) - 0.5 * ln_sigma_2 + ln_p_w2
+
+    # calculate g_i
     g_1 = x_Wi_x_1 + w1_t_x + w_10
     g_2 = x_Wi_x_2 + w2_t_x + w_20
 
